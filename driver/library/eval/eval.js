714600be5cfefccfaa83d83dc76f19ba75826c93
megamind <megamindbrian@gmail.com>
2022-05-07 13:59:16 -0700

    better eval formatting

--- driver/library/eval/eval.js
+++ driver/library/eval/eval.js
@@ -7,51 +7,60 @@ let programResponses = []
 let programHeap = []
 
 const BOOTSTRAP_CURSOR = 'script'
-const BOOTSTRAP_EVAL = `(
+// using this silly template because no matter
+//   what somebody passes from front-end, it gives
+//   me the same expression result. it also allows
+//   me to use `return` statement inside a "virtual"
+//   global section without affecting variables outside
+//   of scope, and it allows me to pop the last result 
+//   off of the returnStack. this concept was taken 
+//   from the ijupyter nodejs kernel. except there, it's 
+//   actually using eval, where-as here, its using a 
+//   visitor and hacking the scope for real references.
 // add aspects to control program stack
-// @Add(@Program,doBootstrap)
-function () {
-	${BOOTSTRAP_CURSOR}
-})()`
+const BOOTSTRAP_EVAL = `( // @Add(@Program,doBootstrap)
+function () { ${BOOTSTRAP_CURSOR} })()`
 const PREAMBLE_LINES = BOOTSTRAP_EVAL
 		.split(BOOTSTRAP_CURSOR)[0]
 		.split('\n').length
 
-
+// I had kind of suspected acorn or something has provided a
+//   `visitor`. this will make it easy to switch to antlr,
+//   only replace binding function for callback. then focus
+//   on mapping symbol names to pass into javascript parser
+//   to reconstruct the correct AST from doing to opposite
+//   of this attribute feature supplement we're doing to the
+//   stack to generalize it in the first place. do the reverse
+//   same as we have onEval, doEval, onMessage, sendMessage
+//   onTranspile, doTranspile is make stack change, and reverse 
+//   stack change back to language.
 function doEval(evalStr) {
-	if(!evalStr) {
-		throw new Error('No program!')
-	}
-	if(evalStr.hasOwnProperty('length')) {
-		return Promise.resolve(onInstruction(evalStr))
-	}
-	if(typeof evalStr != 'string') {
-		throw new Error('Don\'t know what to do!')
-	}
 	let accumulatedComments = []
 	let comments = []
-	let program = acorn.parse(BOOTSTRAP_EVAL.replace('script', evalStr),
-	{	
+	let program = acorn.parse(
+		BOOTSTRAP_EVAL.replace('script', evalStr), {	
 		ecmaVersion: 2020, locations: true, 
-		onToken: doComments,
-		onComment: onComments,
+		onToken: doComment.bind(comments, accumulatedComments),
+		onComment: onComment.bind(comments, accumulatedComments),
 	})
-	program.comments = comments
 	// make sure we hit a `Program` first thing
-	return onInstruction(doInstruction(evalStr))
+	walk.full(program, 
+		// pass to infix to postfix stack creator
+		doInstruction.bind(null, programCallstack, comments))
+	// await (Promise) for program to finish executing
+	return onInstruction(programCallstack, program)
+		.then(onEval)
 }
-
-// pretty loose attribute parser, parses attribs with 1 or more params
-//  like @Function(_bootstrap,doBootstrap)
-
-const MATCH_ATTRIBUTE = /@(add|remove)\s*\(\s*([^,\)]*?)\s*(,\s*[^,\)]*?\s*)*\)/i
 
 // every time I feel like I can make a vast improvement in style
 // to test this concept, lets use it from the very beginning
 
-function onInstruction(runContext, resolve, reject) {
+// onInstruction awaits until stack is cleared
+function onInstruction() {
+	// TODO: return a new call frame
 	if(!runContext.programTimer) {
-		runContext.programTimer = setInterval(runStatement.bind(null, runContext, resolve, reject))
+		runContext.programTimer = setInterval(
+			runStatement.bind(null, runContext, resolve, reject))
 	}
 	if(stack.length < frame - 1) {
 		clearInterval(runContext.programTimer)
@@ -63,230 +72,49 @@ function onInstruction(runContext, resolve, reject) {
 		} else {
 			return resolve(runContext.bubbleReturn.pop())
 		} 
-	} else {
-		doAttributes(programCallstack.pop(), 
-		{programCallstack, callFrame, runContext})
-	}
-
-}
-
-function doInstruction() {
-	programCallstack.push({
-		type: 'Evaluate',
-		value: (function (instruction) {
-			onEval(runContext, instruction)
-			debugger
-			throw new Error(abstractNode.type + ': Not implemented!')
-		}).bind(null, abstractNode)
-	})
-	// TODO: return program context if one does not exist
-}
-
-// TODO: write in a way we can add attribution to other things like MP3s
-function doAttributes() {
-	let comments = runContext.comments[abstractNode.start] || []
-	if(typeof comments == 'string') {
-		comments = [runContext.comments[abstractNode.start]]
-	}
-	for(let i = 0; i <= comments.length; i++) {
-		let attribName
-		let params = []
-		if(i == comments.length) {
-			attribName = '@'+abstractNode.type.toLocaleLowerCase()
-		}
-	}
-	// TODO: push Attributes to stack
-	// do instruction
-}
-
-
-function onAttributes() {
-	// on instructions
-	// actually to do the attribute thing
-}
-
-
-function doComments(abstractNode) {
-	let match = MATCH_ATTRIBUTE.exec(runContext.comments[abstractNode.start])
-	if(match) {
-		attribName = match[1].toLocaleLowerCase()
-		params = match[3].split(/\s*,\s*/g).slice(1)
-		params.unshift(match[2])
-	}
-	comments[abstractNode.start] = accumulatedComments
-	accumulatedComments = []
-	for(let j = 0; j < runContext.attributes[attribName].length; j++) {
-		let callee = runContext.attributes[attribName][j]
-		if(typeof callee == 'string') {
-			callee = runContext[runContext.attributes[attribName][j]]
-		}
-		// TODO: push onto stack using same mechanisms
-		callee(params, {abstractNode, programCallstack, callFrame, runContext})
-		// TODO: doAttributes
 	}
 }
-
-
-function onComments(_, comment, start, end) {
-	debugger
-	accumulatedComments.push(comment)
-	// TODO: doComments
-}
-
 
 // TODO: get rid of repl/accessor
 function onEval(node) {
 	if(node && node.type == 'Evaluate') {
 		return Promise.resolve(node.value())
 	}
-	return {
-		result: ctx.bubbleReturn[0]
-	}
+	// TODO: manage call stack / return stack, that's all
+	return ctx.bubbleReturn[0]
 }
 
-function doStatments(runContext, program) {
-	return new Promise(function (resolve, reject) {
-		programThreads.push(runContext)
-		programResponses.push({resolve, reject})
-		if(typeof runContext.bubbleReturn == 'undefined') {
-			runContext.bubbleReturn = new Array(programThreads.length).fill(void 0)
-		} else {
-			runContext.bubbleReturn.push(void 0)
-			console.assert(runContext.bubbleReturn.length == programThreads.length)
-		}
-		if(typeof runContext.attributes == 'undefined') {
-			runContext.attributes = {
-				'add': [
-					doAdd
-				],
-				'remove': [
-					doRemove
-				],
-				'@evaluate': [
-					onEval
-				]
-			}
-		}
-		runContext.doBootstrap = doBootstrap
-		runContext.programTimer = setInterval((
-		function ({programCallstack, callFrame, runContext}) {
-			if(doBubbleOut(programCallstack, callFrame, runContext)) {
-				programThreads.splice(programThreads.indexOf(runContext), 1)
-				
-			} else {
-				
-			}
-		}).bind(null, {
-			programCallstack: program.hasOwnProperty('length') ? program : [program],
-			callFrame: programThreads.length,
-			runContext: runContext,
-		}), 1000/60)
+// doInstruction pushes AST arguments/left/right 
+//   onto stack synchronously as a result of every 
+//   async eval or the natural await stack.pop() 
+//   at specified FPS. It's a program animator.
+// Step by step calculator? this a basic stack based
+//   calculator, except parentheses operators has already
+//   been simplified for us via the walk.full()
+// If we load the full stack infix-posfix the 
+//   programCallstack is actually just a list with 
+//   a new mirror of doInstruction push operations, 
+//   makes it easy to isolate single call-frames 
+//   when debugging.
+function doInstruction(runContext, resolve, reject) {
+	//   higher level language? software nuero-lingistic 
+	//   programming?
+	// off the program stack, on to call stack, 
+	//   same as in CPU memory, but at the...
+	let abstractNode = programCallstack.pop()
+	programCallstack.push({
+		type: 'Evaluate',
+		value: onEval.bind(null, abstractNode)
 	})
 }
 
-function doBubbleOut(stack, frame, runContext) {
-	
-}
+// PROGRAM CALCULATOR IN < 100 LOC WAS 
+//   MY GOAL AND MOST OF IT IS COMMENTS.
+// Why does this matter? 
+// Because the [docs](./docs/eval.md) matches the code
+//   exactly. It literally does nothing more than what 
+//   I said it was going to do. Everything is self
+//   contained inside a functional context, to add
+//   REPL features would be the same simple middle-
+//   ware pattern to add specific data-types/JSON/ZMQ/COM
 
-function doAttributes(abstractNode, {programCallstack, callFrame, runContext}) {
-	 /* else if (i == comments.length + 1) {
-			attribName = 
-		} */ else {
-			 else {
-				continue
-			}
-		}
-		if(typeof runContext.attributes[attribName] == 'undefined') {
-			continue
-		}
-		
-	}
-	
-}
-
-function doAdd(attributeParams, {abstractNode, runContext}) {
-	if(typeof runContext.alreadyAttributed == 'undefined') {
-		runContext.alreadyAttributed = []
-	}
-	if(runContext.alreadyAttributed[abstractNode.start]) {
-		return
-	}
-	runContext.alreadyAttributed[abstractNode.start] = true
-	if(typeof runContext.attributes[attributeParams[0].toLocaleLowerCase()] == 'undefined') {
-		runContext.attributes[attributeParams[0].toLocaleLowerCase()] = []
-	}
-	runContext.attributes[attributeParams[0].toLocaleLowerCase()].push(attributeParams[1])
-}
-
-function doRemove(attributeParams, {runContext}) {
-	if(typeof runContext.attributes[attributeParams[0].toLocaleLowerCase()] == 'undefined') {
-		delete runContext.attributes[attributeParams[0].toLocaleLowerCase()]
-	}
-}
-
-const PROGRAM_EXPRESSIONS = [
-	'Program', 'Unsyntactic', 'BreakStatement', 'ContinueStatement',
-	'DebuggerStatement', 'DoWhileStatement', 'VariableDeclaration', 'IfStatement',
-	'ReturnStatement', 'SwitchCase', 'SwitchCase', 'SwitchStatement',
-	'ThrowStatement', 'CatchClause', 'TryStatement', 'VariableDeclaration',
-	'WhileStatement', 'WithStatement', 'EmptyStatement', 'LabeledStatement',
-	'ExpressionStatement', 'BlockStatement', 'ForStatement', 'ForInStatement',
-	'ForOfStatement', 'VariableDeclarator', 'FunctionDeclaration', 'FunctionExpression',
-	'ClassBody', 'ClassDeclaration', 'ClassExpression', 'Identifier',
-	'MethodDefinition', 'PropertyDefinition', 'StaticBlock', 'ExportAllDeclaration',
-	'ExportDefaultDeclaration', 'ExportNamedDeclaration', 'ExportSpecifier', 'ImportDeclaration',
-	'ImportDefaultSpecifier', 'ImportNamespaceSpecifier', 'ImportSpecifier', 'SpreadElement',
-	'RestElement', 'ArrayPattern', 'AssignmentPattern', 'SequenceExpression',
-	'AssignmentExpression', 'ConditionalExpression', 'LogicalExpression', 'BinaryExpression',
-	'UpdateExpression', 'UnaryExpression', 'UpdateExpression', 'ChainExpression',
-	'MemberExpression', 'CallExpression', 'TaggedTemplateExpression', 'Super',
-	'ThisExpression', 'Literal', 'ArrayExpression', 'ImportExpression',
-	'MetaProperty', 'Literal', 'SequenceExpression', 'ParenthesizedExpression',
-	'MetaProperty', 'NewExpression', 'TemplateElement', 'TemplateLiteral',
-	'ObjectPattern', 'ObjectExpression', 'RestElement', 'SpreadElement',
-	'Property', 'FunctionExpression', 'ArrowFunctionExpression', 'Identifier',
-	'PrivateIdentifier', 'YieldExpression', 'AwaitExpression',
-]
-
-function doBootstrap(attributeParams, {runContext}) {
-	if(runContext.doneBootstrap) {
-		return
-	}
-	runContext.doneBootstrap = true
-	for(let i = 0; i < PROGRAM_EXPRESSIONS.length; i++) {
-		let newAttribName = '@' + PROGRAM_EXPRESSIONS[i].toLocaleLowerCase()
-		if(typeof runContext.attributes[newAttribName] == 'undefined') {
-			runContext.attributes[newAttribName] = []
-		}
-	
-		if(typeof runContext['do' + PROGRAM_EXPRESSIONS[i]] == 'function') {
-			runContext.attributes[newAttribName].push(
-				runContext['do' + PROGRAM_EXPRESSIONS[i]]
-			)
-		} else {
-			runContext.attributes[newAttribName].push(
-				doInstruction
-			)
-		}
-	}
-}
-
-function onEval(attributeParams, 
-	{abstractNode, programCallstack, runContext}) {
-	debugger
-	console.log('Thread ' + runContext.programTimer, 'Evaluate.' + node.type)
-	//console.log(...exp)
-}
-
-function doInstruction(attributeParams, 
-		{abstractNode, programCallstack, runContext}
-) {
-	// TODO: if type == Evaluate, await
-	debugger
-	
-	//
-}
-
-//function doFunctionExpression(attributeArg, attributeParams, abstractNode, runContext) {
-//	debugger
-//}
