d46bdea5d74fbd7f2975899c3c10ba933d934531
megamind <megamindbrian@gmail.com>
2022-05-11 15:04:15 -0700

    organize

---
+++ driver/library/cerebro/patch-adams.js
@@ -0,0 +1,24 @@
+// Get it? software-patch, patch-adams medicine related.
+
+// patch my medical career into a system of memorization
+//   and study with the spaced-repition module
+
+// TODO: then patch this system into data systems and 
+//   fuck the current insurance system by under-cutting
+//   the market with healthy policy holders using a $500
+//   marketing loss-leader for massage chairs to help
+//   diabetics with blood-flow on their PT-offdays.
+// Just like the Thaiwanese (massagers). LOL
+
+// TODO: then purchase and sell insurance companies like candy.
+
+// TODO: I always thought the idea of "we're all in this together"
+//   and privacy were in conflict with eachother when it comes
+//   to medicine. Expose medical data in a way where the
+//   general public can be in competition with each other
+//   to fight unhealth norms, i.e. a city wide insurance
+//   policy where rules like "NYC soda-pop law" actually 
+//   leads to lower taxes because the insurance policy
+//   for the city is costing less due to less sugar intake.
+
+
