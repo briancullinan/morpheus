254948b7f06f014ba4868f0bd94b66526a9bd237
megamind <megamindbrian@gmail.com>
2022-05-11 12:00:50 -0700

    better framework declarations and notes

--- driver/library/index.js
+++ driver/library/index.js
@@ -1,19 +1,34 @@
+// THEORY
+/*
+By writing this entire REPL / @Attributes
+extension for nodeJS, I'll be able to solve
+any complexity of a visual communications 
+problem, my code base can grow infinitely
+without growing in complexity failling short
+of usability. AND! as a side-effect, I'll never
+need to write a component more than 1 or 2 
+functions and some declarative framework code.
+*/
 
+// I.E.
+// @Template
+// @Framework(ideologies)
+function framework(name) {
+	bootstrap
 
-// It might be nice if I could just arbitrarily include code
-//   in my page without like having to write <script src="" />
-// TODO: SCSS compiler is/was native C, by now it's probably WASM.
-//   but it might be nice to use that in templates with needing
-//   `npm install scss` and 100,000 LoC with webpack. maybe a 
-//   wrapper to convert basic SCSS structures to their output.
+	load(env)
 
-// TODO: auto-detect some language features and output a module
-//   that can be run in different contexts. i.e. Makefile / doEval('./Makefile')
-//   all could pass through a single require statement.
-// but how to define it?
+	init
+}
+// should be able to load wordpress, ourselves, symphony projects, 
+//   old drupal projects, phpBB, deployment frameworks, etc.
+
 /*
 
 */
+
+// TODO: parse our own file using the @Attribute system to load the REPL framework
+
 function bootstrap({library, require}, libraryFile) {
 	// TODO: move require and __library down below libraryLookup
 	if(typeof require != 'undefined') {
@@ -41,7 +56,59 @@ function bootstrap({library, require}, libraryFile) {
 	//   return a template to replace middleware components
 
 }
+// ^^^ should be able to load wordpress, ourselves, symphony projects, 
+//   old drupal projects, phpBB, deployment frameworks, etc.
 
+
+// I.E.
+// to consume all Google cloud services, 
+//    the declaration in cloud.js becomes something like:
+({
+boostrap: ['gutil', 'https://google.apis'],
+load: middleware(['express', 'google cloud function mock']),
+init: ['npm', framework] // boot itself in outer context
+})
+// TODO: same for AWS, because framework() is a consumer of
+//   framework definition files these cloud providers provide
+//   either in the form of consumable REST documentation, OR
+//   better yet, native bindings for one of our supported REPL
+//   languages. Which means with one more declaration, and can
+//   harmoniously connect C# to PHP, JS to Node-GYP definitions, etc.
+({
+
+})
+
+// TODO: one more proof of concept of convert attribute code to jupyter notebooks
+({
+	
+})
+
+// TODO: on more proof of concept to generate a google docs style website with 
+//    ace editor and live cloud coding using 5 lines of code here
+({
+
+})
+
+// TODO: bootstrap old PHP project into PHP running jupyter kernel, 
+//   then bootstrap running jupyter kernel with our own REPL service.
+// TODO: write kernel.js declarations first, LOL, should output kernel 
+//   installation file, run startup, and call API, as a template, reverse
+//   the flow of our own REPL where onConsole from jupyter calls doConsole() inside doEval(), LOL
+({
+
+})
+
+// It might be nice if I could just arbitrarily include code
+//   in my page without like having to write <script src="" />
+// TODO: SCSS compiler is/was native C, by now it's probably WASM.
+//   but it might be nice to use that in templates with needing
+//   `npm install scss` and 100,000 LoC with webpack. maybe a 
+//   wrapper to convert basic SCSS structures to their output.
+
+// TODO: auto-detect some language features and output a module
+//   that can be run in different contexts. i.e. Makefile / doEval('./Makefile')
+//   all could pass through a single require statement.
+// but how to define it?
 
 // TODO: move to repl/env.js
 if(typeof module != 'undefined') {
