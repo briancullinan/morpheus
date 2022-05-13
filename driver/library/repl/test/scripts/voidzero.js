277e12d5cf68d2be8513e6b4478f1474741259d7
megamind <megamindbrian@gmail.com>
2022-05-12 21:06:05 -0700

    void zero notes

--- driver/library/repl/test/scripts/voidzero.js
+++ driver/library/repl/test/scripts/voidzero.js
@@ -1,18 +1,18 @@
 // A traditional framework is something like this:
 
 // @Service
-function myService() {
-
+function service() {
+  functionBody
 }
 
 // @Main
-function myAction() {
-  let result = myService()
+function action() {
+  let result = service()
   send(result)
 }
 
-function webEvent() {
-  myAction()
+function event() {
+  action()
 }
 
 // this is not how Void Zero works best. in fact,
@@ -33,11 +33,23 @@ bootstrap: myService,
 load: myAction,
 init: webEvent(),
 })
-// ^^^ missing send() is injected by REPL context (not shown here)
+// ^^^ missing send() is injected by REPL context 
+//   (not shown here)
 
 // this is how Void Zero specializes. tools to rewrite
 //   and analyze code starts by transforming the context
 //   we program in.
 
+// the goal of void zero is to help you never have to
+//   write this scaffolding again. the author never 
+//   needs to call myAction or myService directly because
+//   they are implied by the framework. to finalize
+//   this concept, we take our service defined above
+//   and inject new inputs and outputs.
+// @Quine
+({
+quine: [__filename, express, json, displayLibrary]
+})
 
-
+// now you should have a json-database service that
+//   draws our entire library project structure in 3D.
